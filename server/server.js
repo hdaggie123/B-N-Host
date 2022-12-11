6be56d78600df3773b84acb3e3ebd9b60f7c5d9b
hdaggie123 <84333215+hdaggie123@users.noreply.github.com>
2022-12-10 21:40:46 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const db = require('./queries')
 const port = 3001
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://barnesandnoblecsce315-frontend.onrender.com');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
